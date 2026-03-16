34179cd58988f9662d78b6394042c6abc28f69af
emgallego <emgallego@D122604.uao.edu.co>
2026-03-16 10:18:50 -0500

    mi primer commit

---
+++ src/main/java/com/mycompany/gitproyectomaven/GitProyectoMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitproyectomaven;
+
+/**
+ *
+ * @author emgallego
+ */
+public class GitProyectoMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
