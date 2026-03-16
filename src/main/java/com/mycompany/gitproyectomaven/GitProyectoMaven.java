f3702d1447fd1844e53ae3f2cd016129dba60146
emgallego <emgallego@D122604.uao.edu.co>
2026-03-16 10:36:32 -0500

    mi tercer commit en la rama main

--- src/main/java/com/mycompany/gitproyectomaven/GitProyectoMaven.java
+++ src/main/java/com/mycompany/gitproyectomaven/GitProyectoMaven.java
@@ -13,5 +13,6 @@ public class GitProyectoMaven {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("esto es un cambio");
+        System.out.println("esto es un cambio2");
     }
 }
